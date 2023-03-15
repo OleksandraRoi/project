87611dc247a0e38c2ee9f0b6c3d0c13f79d9738d
Maksym <mfxmfx81@gmail.com>
2023-03-15 16:03:52 -0400

    Added pages folder

---
+++ src/test/java/tests/Title_testCase1.java
@@ -0,0 +1,9 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+import utils.Driver;
+@Test
+public class Title_testCase1 {
+
+}
--- src/test/java/utils/ConfigReader.java
+++ src/test/java/utils/ConfigReader.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Properties;
-
 public class ConfigReader {
 
     private static final Properties properties;
