d75087d00c09051f5848775f5e6eaebdb74e0209
Katy <katia06@gmail.com>
2023-03-20 15:38:03 -0400

    creted swich to another URL and assert the URL

--- src/test/java/tests/FindSocialIcons.java
+++ src/test/java/tests/FindSocialIcons.java
@@ -13,128 +13,175 @@ import utils.Driver;
 
 import javax.print.attribute.HashPrintServiceAttributeSet;
 
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
+
 import static org.testng.Assert.assertTrue;
 import static org.testng.AssertJUnit.assertEquals;
 
 
 public class FindSocialIcons extends TestBase {
 
+
         @Test
     public void testPinterestIcon() throws InterruptedException {
 
+
             WebElement pinterestIcon = Driver.getDriver().findElement(By.cssSelector("a[title='Pinterest']"));
+           // check if the Pinterest icon is displayed
+             assertTrue(pinterestIcon.isDisplayed());
             pinterestIcon.click();
 
-           // Driver.getDriver().findElement(By.cssSelector("a[title='Pinterest']")).sendKeys(Keys.CONTROL+"\t");
+            // Swich pages
+            Set<String> handles =  Driver.getDriver().getWindowHandles();//parentid,childid
+            Iterator it = handles.iterator();
+            String parentid = (String) it.next();
+            String childid = (String) it.next();
 
-            Driver.getDriver().switchTo().window("https://www.pinterest.com/flooranddecor/");
-            // check if the Pinterest icon is displayed
-             assertTrue(pinterestIcon.isDisplayed());
-//            String currentUrl = Driver.getDriver().getCurrentUrl();
-//            String expectedUrl = "https://www.pinterest.com/flooranddecor/";
-//            Assert.assertEquals(currentUrl, "https://www.pinterest.com/flooranddecor/");
+            Driver.getDriver().switchTo().window( childid );
+
+          String currentUrl = Driver.getDriver().getCurrentUrl();
+          String expectedUrl = "https://www.pinterest.com/flooranddecor/";
+              Assert.assertEquals(currentUrl, "https://www.pinterest.com/flooranddecor/");
         }
 
 
 
     @Test
     public void testSocialInstagramIcon() throws InterruptedException {
-//        WebDriver driver = new EdgeDriver();
-//        driver.manage().window().maximize();
-//
-//        // Navigate to the website
-//        driver.get("https://www.flooranddecor.com/");
-//
+
+
 //        // Find the Instagram icon in the footer and click it
         WebElement instagramIcon = Driver.getDriver().findElement(By.cssSelector("a[title='Social Instagram Link']"));
-
-        instagramIcon.click();
         //  check if the Instagram icon is displayed
         assertTrue(instagramIcon.isDisplayed());
 
+        instagramIcon.click();
+
+     // Swich pages
+        Set<String> handles =  Driver.getDriver().getWindowHandles();//parentid,childid
+        Iterator it = handles.iterator();
+        String parentid = (String) it.next();
+        String childid = (String) it.next();
+
+        Driver.getDriver().switchTo().window( childid );
+
+        String currentUrl = Driver.getDriver().getCurrentUrl();
+        String expectedUrl = "https://www.pinterest.com/flooranddecor/";
+        Assert.assertEquals(currentUrl, "https://www.instagram.com/flooranddecor/");
 
     }
 
     @Test
 
     public void testFacebookIcon() {
-//        WebDriver driver = new EdgeDriver();
-//        driver.manage().window().maximize();
-//
-//        // Navigate to the website
-//        driver.get("https://www.flooranddecor.com/");
-//        // Find the FaceBook icon
+
         WebElement facebookIcon = Driver.getDriver().findElement(By.cssSelector("a[title='FaceBook']"));
-        facebookIcon.click();
+
+
+
         //  check if the FaceBook icon is displayed
         assertTrue(facebookIcon.isDisplayed());
+
+        facebookIcon.click();
+
+        // Swich pages
+        Set<String> handles =  Driver.getDriver().getWindowHandles();//parentid,childid,subchild
+        Iterator it = handles.iterator();
+        String parentid = (String) it.next();
+        String childid = (String) it.next();
+
+        Driver.getDriver().switchTo().window( childid );
+        String currentUrl = Driver.getDriver().getCurrentUrl();
+        String expectedUrl = "https://www.pinterest.com/flooranddecor/";
+        Assert.assertEquals(currentUrl, "https://www.facebook.com/flooranddecor");
+
 
     }
 
     @Test
     public void testYouTubeIcon() {
 
-//        WebDriver driver = new EdgeDriver();
-//        driver.manage().window().maximize();
-//        // Navigate to the website
-//        driver.get("https://www.flooranddecor.com/");
-//        // find the YouTube icon
         WebElement youtubeIcon = Driver.getDriver().findElement(By.cssSelector("a[title='YouTube']"));
-        youtubeIcon.click();
-        // find the YouTube icon
         // check if the YouTube icon is displayed
         assertTrue(youtubeIcon.isDisplayed());
+        youtubeIcon.click();
+        // Swich pages
+        Set<String> handles =  Driver.getDriver().getWindowHandles();//parentid,childid,subchild
+        Iterator it = handles.iterator();
+        String parentid = (String) it.next();
+        String childid = (String) it.next();
+
+        Driver.getDriver().switchTo().window( childid );
+       
+        String currentUrl = Driver.getDriver().getCurrentUrl();
+        String expectedUrl = "https://www.pinterest.com/flooranddecor/";
+        Assert.assertEquals(currentUrl, "https://www.youtube.com/FloorAndDecor");
+
     }
 
     @Test
     public void testSocialLinkedinIcon() throws InterruptedException {
 
-//        WebDriver driver = new EdgeDriver();
-//        driver.manage().window().maximize();
-//        // Navigate to the website
-//        driver.get("https://www.flooranddecor.com/");
-
         // find the Linked In icon
         WebElement socialLinkedinIcon = Driver.getDriver().findElement(By.cssSelector("a[title='Social Linked In Link']"));
-        socialLinkedinIcon.click();
-        // find the Linked In icon
         // check if the Linked In icon is displayed
         assertTrue(socialLinkedinIcon.isDisplayed());
+        socialLinkedinIcon.click();
+        // Swich pages
+        Set<String> handles =  Driver.getDriver().getWindowHandles();//parentid,childid,subchild
+        Iterator it = handles.iterator();
+        String parentid = (String) it.next();
+        String childid = (String) it.next();
+
+        Driver.getDriver().switchTo().window( childid );
+        String currentUrl = Driver.getDriver().getCurrentUrl();
+        String expectedUrl = "https://www.pinterest.com/flooranddecor/";
+        Assert.assertEquals(currentUrl, "https://www.linkedin.com/company/flooranddecor/");
+
     }
 
     @Test
     public void testTwitterIcon() throws InterruptedException {
-//
-//        WebDriver driver = new EdgeDriver();
-//        driver.manage().window().maximize();
-//        // Navigate to the website
-//        driver.get("https://www.flooranddecor.com/");
 
         // find the Twitter Icon
         WebElement twitterIcon = Driver.getDriver().findElement(By.cssSelector("a[title='Twitter']"));
-       twitterIcon.click();
-
         // check if the Twitter icon is displayed
         assertTrue(twitterIcon.isDisplayed());
+       twitterIcon.click();
+        Set<String> handles =  Driver.getDriver().getWindowHandles();//parentid,childid,subchild
+        Iterator it = handles.iterator();
+        String parentid = (String) it.next();
+        String childid = (String) it.next();
 
-        // Assert that the URL is correct
-     //  assertEquals("https://twitter.com/flooranddecor", Driver );
+        Driver.getDriver().switchTo().window( childid );
+        String currentUrl = Driver.getDriver().getCurrentUrl();
+        String expectedUrl = "https://www.pinterest.com/flooranddecor/";
+        Assert.assertEquals(currentUrl, "https://twitter.com/flooranddecor");
+
 
     }
 
     @Test
     public void testSocialHouzzLinkIcon() throws InterruptedException {
-//
-//        WebDriver driver = new EdgeDriver();
-//        driver.manage().window().maximize();
-//        // Navigate to the website
-//        driver.get("https://www.flooranddecor.com/");
 
         // find the Houzz icon
         WebElement socialHouzzLinkIcon= Driver.getDriver().findElement(By.cssSelector("a[title='Social Houzz Link']"));
-        socialHouzzLinkIcon.click();
         // check if the Linked In icon is displayed
         assertTrue(socialHouzzLinkIcon.isDisplayed());
+        socialHouzzLinkIcon.click();
+        // Swich pages
+        Set<String> handles =  Driver.getDriver().getWindowHandles();//parentid,childid,subchild
+        Iterator it = handles.iterator();
+        String parentid = (String) it.next();
+        String childid = (String) it.next();
+
+        Driver.getDriver().switchTo().window( childid );
+        String currentUrl = Driver.getDriver().getCurrentUrl();
+        String expectedUrl = "https://www.pinterest.com/flooranddecor/";
+        Assert.assertEquals(currentUrl, "https://www.houzz.com/professionals/flooring-contractors/floor-and-decor-pfvwus-pf~276889407?");
+
     }
 
 }
