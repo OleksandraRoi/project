7be62d3cf9734a2c9c32993ef0c377e72424fad1
muslima <mismuslimakhon@gmail.com>
2023-03-26 06:38:59 -0400

    commit

---
+++ src/test/java/pages/WoodSection_Page.java
@@ -0,0 +1,69 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utils.Driver;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class WoodSection_Page {
+    public WoodSection_Page() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/test/java/tests/WoodSection_Test.java
@@ -0,0 +1,53 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.ShoppingCart_Page;
+
+public class WoodSection_Test extends TestBase {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
