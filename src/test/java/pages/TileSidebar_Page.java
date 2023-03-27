9ab447be692b236be600f922d8907c0d6e8254a4
Katy <katia06@gmail.com>
2023-03-27 10:19:20 -0400

    Initial commit on 03/27

--- src/test/java/pages/TileSidebar_Page.java
+++ src/test/java/pages/TileSidebar_Page.java
@@ -8,6 +8,8 @@ import tests.TileSidebarTest;
 import utils.Driver;
 import utils.SeleniumUtils;
 
+import java.util.List;
+
 import static org.openqa.selenium.By.className;
 
 public class TileSidebar_Page {
@@ -15,15 +17,27 @@ public class TileSidebar_Page {
     public TileSidebar_Page() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
-       @FindBy (id = "main-nav-tile")
-       private WebElement tilebotton;
+    @FindBy (id = "main-nav-tile")
+    private WebElement tilebotton;
 
+    @FindBy(id = "onetrust-accept-btn-handler")
+    public WebElement acceptAllCookiesButton;
+
+    @FindBy(xpath = "/html/body/div[1]/div[9]/div[1]/div[1]/main/div/aside/div/div/div[2]/div[1]/div[1]")
+    private WebElement coloTab;
 
 
     public void clickTileButton () {
         tilebotton.click();
     }
         public String urlActual () {
-            return Driver.getDriver().getCurrentUrl();
+        return Driver.getDriver().getCurrentUrl();
         }
-    }
+        public void clickColorTab () {
+            coloTab.click();
+        }
+
+
+
+            }
+
--- src/test/java/tests/TileSidebarTest.java
+++ src/test/java/tests/TileSidebarTest.java
@@ -1,20 +1,34 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.ApplyPage;
 import pages.TileSidebar_Page;
+import pages.WoodSection_Page;
 import pages.oneTitlePage;
+import utils.Driver;
+
+import java.util.List;
 
 public class TileSidebarTest extends TestBase {
+    TileSidebar_Page tileSidebar_page = new TileSidebar_Page();
 
     @Test
-    public void tile(){
+    public void tile() throws InterruptedException {
 
-        TileSidebar_Page tileSidebar_page = new TileSidebar_Page();
         tileSidebar_page.clickTileButton();
-       Assert.assertEquals(tileSidebar_page.urlActual(),"https://www.flooranddecor.com/tile");
+        Assert.assertEquals(tileSidebar_page.urlActual(), "https://www.flooranddecor.com/tile");
+        //Thread.sleep(2000);
 
     }
-
+    @Test
+    public void color() throws InterruptedException {
+        tileSidebar_page.clickTileButton();
+        tileSidebar_page.acceptAllCookiesButton.click();
+       // Thread.sleep(2000);
+        tileSidebar_page.clickColorTab();
+        Assert.assertTrue(Driver.getDriver().getPageSource().contains("Color"));
     }
+}
