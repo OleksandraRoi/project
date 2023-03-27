f90a7498ba79695ad398a2b2fe4225a324df3b87
Oleksandra Roi <sashahardy1532@gmail.com>
2023-03-27 15:40:13 -0400

    test 4

--- src/test/java/tests/MoreInfoVisualizerTests.java
+++ src/test/java/tests/MoreInfoVisualizerTests.java
@@ -8,15 +8,21 @@ public class MoreInfoVisualizerTests extends TestBase{
 
     @Test
     public void Visualizer(){
+        logger.info("Navigating to 'Floor & Decor' Home Page");
         MoreInfoVisualizerPage moreInfoVisualizer = new MoreInfoVisualizerPage();
+        logger.info("Accept cookies");
         moreInfoVisualizer.acceptCookies();
         SeleniumUtils.waitForClickablility(moreInfoVisualizer.getPopUpWindow(), 15);
+        logger.info("Close pop up window");
         moreInfoVisualizer.popUpWindowClick();
         SeleniumUtils.waitForClickablility(moreInfoVisualizer.getDecorativesSection(), 5);
+        logger.info("Navigate to Decoratives section");
         moreInfoVisualizer.clickOnDecoratives();
         moreInfoVisualizer.clickOnSideBar();
         moreInfoVisualizer.scrollDown(moreInfoVisualizer.getVisualizerElement());
+        logger.info("Navigate to Visualizer");
         moreInfoVisualizer.visualizerElementClick();
+        logger.info("Get Started button click");
         moreInfoVisualizer.getStartedButtonClcik();
         SeleniumUtils.waitFor(8);
         //moreInfoVisualizer.iframeSwitch();
